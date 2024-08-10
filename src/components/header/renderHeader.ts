29d79733eba1f5da76c9b964328c56a2e13445cd
Guillem Blasco <gowet92@gmail.com>
2024-08-10 20:27:59 +0200

    Add renderNavigatorMenu function and style it

--- src/components/header/renderHeader.ts
+++ src/components/header/renderHeader.ts
@@ -1,10 +1,16 @@
+import renderNavigationMenu from "../navigatorMenu/renderNavigatorMenu.js";
+
 const renderHeader = (): HTMLElement => {
   const header = document.createElement("header");
 
   header.classList.add("header");
 
+  const navigationMenu = renderNavigationMenu();
+
   header.innerHTML = `
-  <h1 class="main-heading">Monuments</h1>`;
+    <h1 class="main-heading">Monuments</h1>
+    ${navigationMenu.outerHTML}
+    `;
 
   return header;
 };
