9d0955ec43e8b160a219b84de150c1ea33ffa2d8
Guillem Blasco <gowet92@gmail.com>
2024-08-10 19:42:21 +0200

    Add header component, eslint and linstaged configuration

---
+++ src/components/header/renderHeader.ts
@@ -0,0 +1,12 @@
+const renderHeader = (): HTMLElement => {
+  const header = document.createElement("header");
+
+  header.classList.add("header");
+
+  header.innerHTML = `
+  <h1 class="main-heading">Monuments</h1>`;
+
+  return header;
+};
+
+export default renderHeader;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import renderApp from "./components/app/renderApp.js";
 
+const root = document.querySelector(".root");
+
+if (!root) {
+  throw new Error("Root element not found");
+}
+
+const app = renderApp();
+
+root.appendChild(app);
