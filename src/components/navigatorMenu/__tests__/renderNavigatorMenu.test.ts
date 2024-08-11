16eccbf3032f41b592ad597f96b589fcab2435ad
Guillem Blasco <gowet92@gmail.com>
2024-08-11 12:29:42 +0200

    Add header and navigatorMenu test

---
+++ src/components/navigatorMenu/__tests__/renderNavigatorMenu.test.ts
@@ -0,0 +1,17 @@
+import renderNavigationMenu from "../renderNavigatorMenu";
+
+describe("Given the navigator menu component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show List Monuments as text", () => {
+      const screen = document.createElement("div");
+      const expectedTextListMonuments = "List Monuments";
+      const navigatorMenu = renderNavigationMenu();
+
+      screen.appendChild(navigatorMenu);
+      const listItem = screen.querySelector("li");
+
+      expect(listItem).not.toBeNull();
+      expect(listItem?.textContent).toBe(expectedTextListMonuments);
+    });
+  });
+});
