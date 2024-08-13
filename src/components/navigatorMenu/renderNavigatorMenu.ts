f315b2fae52f77ffd3691a984779d9be6e8e85c2
Guillem Blasco <gowet92@gmail.com>
2024-08-13 16:49:51 +0200

    Refactor navigator-menu styles

--- src/components/navigatorMenu/renderNavigatorMenu.ts
+++ src/components/navigatorMenu/renderNavigatorMenu.ts
@@ -3,9 +3,9 @@ const renderNavigatorMenu = (): HTMLElement => {
 
   navigatorMenu.classList.add("navigator-menu");
   navigatorMenu.innerHTML = `
-    <ul class="navigator-menu__list">
-      <li class="navigator-menu__item--active"><a href="">List Monuments</a></li>
-      <li class="navigator-menu__item--active"><a href="">Create monument</a></li>
+    <ul class="navigator-list">
+      <li class="navigator-list__item navigator-list__item--active"><a>List Monuments</a></li>
+      <li class="navigator-list__item "><a>Create monument</a></li>
     </ul>
   `;
 
