4051c29489d658c34f83986f51ba1a835bec172c
Guillem Blasco <gowet92@gmail.com>
2024-08-15 23:56:54 +0200

    Relocate favicon and remove compiled code

--- public/js/app/renderApp.js
+++
@@ -1,7 +0,0 @@
-const renderApp = () => {
-    const app = document.createElement("div");
-    app.classList.add("app-container");
-    return app;
-};
-export default renderApp;
-//# sourceMappingURL=renderApp.js.map
