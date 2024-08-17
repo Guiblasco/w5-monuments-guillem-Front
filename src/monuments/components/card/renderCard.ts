414087275484995d631a4a731000a5defde7cea1
Guillem Blasco <gowet92@gmail.com>
2024-08-17 22:02:44 +0200

    Add function renderCard

---
+++ src/monuments/components/card/renderCard.ts
@@ -0,0 +1,13 @@
+import type { Monument } from "../../types";
+
+export const renderCard = (monument: Monument): HTMLElement => {
+  const card = document.createElement("article");
+
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <h2>${monument.name}</h2>
+  `;
+
+  return card;
+};
